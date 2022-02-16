2baac870f470e295cdd4cca68dcd6d56afb661df
seolheeson <elma1673@naver.com>
2022-02-16 10:10:58 +0900

    Mysql-Create one to one relation

---
+++ class/16-01-mysql-one-to-one/src/apis/product/entities/product.entity.ts
@@ -0,0 +1,31 @@
+import { ProductSaleslocation } from 'src/apis/productSaleslocation/entities/productSaleslocation.entity';
+import {
+  Column,
+  Entity,
+  JoinColumn,
+  OneToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+@Entity()
+export class Product {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  description: string;
+
+  @Column()
+  price: number;
+
+  @Column()
+  isSoldout: boolean;
+  // soldedAt: Date
+
+  @JoinColumn()
+  @OneToOne(() => ProductSaleslocation)
+  productSaleslocation: ProductSaleslocation;
+}
